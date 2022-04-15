4f6d97c365a49bc7bc1c9c3ccb9ef09f85b52dd8
rasovarsenii <rasovarsenii@yandex.ru>
2022-04-15 13:07:33 +0500

    new func

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 def fu():
-    print('fu')
+    return
 
-    
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
